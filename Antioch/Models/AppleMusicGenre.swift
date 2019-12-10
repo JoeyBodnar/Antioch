2134799cb3695b1ba8007fd440db8d1ae4417d73
Stephen Bodnar <stephenbodnar@Stephens-MacBook-Pro.local>
2019-12-11 00:00:47 +0700

    add more genres

--- Antioch/Models/AppleMusicGenre.swift
+++ Antioch/Models/AppleMusicGenre.swift
@@ -18,7 +18,7 @@ public class AppleMusicGenre {
     }
     
     public static var allGenres: [AppleMusicGenre] {
-        let all = [AppleMusicGenre(id: 29, name: GenreName.anime), AppleMusicGenre(id: 20, name: GenreName.alternative), AppleMusicGenre(id: 2, name: GenreName.blues), AppleMusicGenre(id: 3, name: GenreName.comedy), AppleMusicGenre(id: 4, name: GenreName.childrens), AppleMusicGenre(id: 5, name: GenreName.classical), AppleMusicGenre(id: 6, name: GenreName.country), AppleMusicGenre(id: 7, name: GenreName.electronic), AppleMusicGenre(id: 19, name: GenreName.world), AppleMusicGenre(id: 18, name: GenreName.hipHopRap), AppleMusicGenre(id: 16, name: GenreName.soundtrack), AppleMusicGenre(id: 10, name: GenreName.singerSongwriter), AppleMusicGenre(id: 14, name: GenreName.pop), AppleMusicGenre(id: 12, name: GenreName.latino), AppleMusicGenre(id: 21, name: GenreName.rock), AppleMusicGenre(id: 24, name: GenreName.reggae), AppleMusicGenre(id: 27, name: GenreName.jPop), AppleMusicGenre(id: 51, name: GenreName.kPop), AppleMusicGenre(id: 0, name: GenreName.all),  AppleMusicGenre(id: 8, name: GenreName.holiday)]
+        let all = [AppleMusicGenre(id: 29, name: GenreName.anime), AppleMusicGenre(id: 20, name: GenreName.alternative), AppleMusicGenre(id: 2, name: GenreName.blues), AppleMusicGenre(id: 3, name: GenreName.comedy), AppleMusicGenre(id: 4, name: GenreName.childrens), AppleMusicGenre(id: 5, name: GenreName.classical), AppleMusicGenre(id: 6, name: GenreName.country), AppleMusicGenre(id: 7, name: GenreName.electronic), AppleMusicGenre(id: 19, name: GenreName.world), AppleMusicGenre(id: 18, name: GenreName.hipHopRap), AppleMusicGenre(id: 16, name: GenreName.soundtrack), AppleMusicGenre(id: 10, name: GenreName.singerSongwriter), AppleMusicGenre(id: 14, name: GenreName.pop), AppleMusicGenre(id: 12, name: GenreName.latino), AppleMusicGenre(id: 21, name: GenreName.rock), AppleMusicGenre(id: 24, name: GenreName.reggae), AppleMusicGenre(id: 27, name: GenreName.jPop), AppleMusicGenre(id: 51, name: GenreName.kPop), AppleMusicGenre(id: 0, name: GenreName.all),  AppleMusicGenre(id: 8, name: GenreName.holiday), AppleMusicGenre(id: 11, name: GenreName.jazz), AppleMusicGenre(id: 22, name: GenreName.christian)]
         return all.sorted(by: { (chart1, chart2) -> Bool in
             return chart1.name < chart2.name
         })
@@ -48,5 +48,6 @@ public struct GenreName {
     public static let latino = "Latino"
     public static let news = "News and Sports"
     public static let holiday = "Holiday"
+    public static let jazz = "Jazz"
 }
 
