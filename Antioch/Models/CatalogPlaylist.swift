c533362e4825970a1e3180b6da7ee087185a63af
Stephen Bodnar <stephenbodnar@Stephens-MacBook-Pro.local>
2019-12-10 21:05:15 +0700

    playParams is now optional

--- Antioch/Models/CatalogPlaylist.swift
+++ Antioch/Models/CatalogPlaylist.swift
@@ -32,7 +32,7 @@ public class CatalogPlaylistAttributes: Decodable {
     public let curatorName: String
     public let lastModifiedDate: String?
     public let name: String
-    public let playParams: PlayParameters
+    public let playParams: PlayParameters?
     public let playlistType: String
     public let url: String
 }
