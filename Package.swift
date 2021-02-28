727f17e04461d7ca9534b9c972df81f49b8c356d
Stephen Bodnar <stephenbodnar@Stephens-MacBook-Pro.local>
2021-02-28 14:38:39 +0700

    rename to AppleMusicKit

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "Antioch",
+    name: "AppleMusicKit",
     platforms: [.iOS("11.0"), .macOS(SupportedPlatform.MacOSVersion.v10_12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Antioch",
-            targets: ["Antioch"]),
+            name: "AppleMusicKit",
+            targets: ["AppleMusicKit"]),
     ],
     dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Antioch",
+            name: "AppleMusicKit",
             dependencies: []),
         .testTarget(
-            name: "AntiochTests",
-            dependencies: ["Antioch"]),
+            name: "AppleMusicKitTests",
+            dependencies: ["AppleMusicKit"]),
     ]
 )
