1bae861b724ee290dd912b75e6411d1294356055
Stephen Bodnar <stephenbodnar@Stephens-MacBook-Pro.local>
2021-03-21 21:14:13 +0700

    begin add resource for unit tests

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,17 +8,16 @@ let package = Package(
     platforms: [.iOS("11.0"), .macOS(SupportedPlatform.MacOSVersion.v10_12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "AppleMusicKit",
-            targets: ["AppleMusicKit"]),
-    ],
+        .library(name: "AppleMusicKit", targets: ["AppleMusicKit"])],
     dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AppleMusicKit",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Test.json")]
+        ),
         .testTarget(
             name: "AppleMusicKitTests",
             dependencies: ["AppleMusicKit"]),
--- Tests/AppleMusicKitTests/AppleMusicKitTests.swift
+++ Tests/AppleMusicKitTests/AppleMusicKitTests.swift
@@ -17,4 +17,61 @@ final class AppleMusicKitTests: XCTestCase {
         XCTAssertEqual(amKit.requestInterceptor.authorizationToken, "mock-jwt")
         XCTAssertEqual(amKit.storeFront, "de")
     }
+    
+    func testPerformRequestForVoidResponseMalformedRequest() {
+        let amKit: AppleMusicKit = AppleMusicKit(session: MockURLSession(), dispatchQueue: .main)
+        
+        var malformedRequestInvoked = false
+        amKit.performRequestforVoidResponse(request: nil) { (result: Result<Void, AppleMusicKitError>) in
+            switch result {
+            case .success: break
+            case .failure(let error):
+                switch error {
+                case .malformedRequest: malformedRequestInvoked = true
+                default: break
+                }
+            }
+        }
+        
+        XCTAssertTrue(malformedRequestInvoked)
+    }
+    
+    func testPerformRequestForVoidResponseApiError() {
+        let mockSession: MockURLSession = MockURLSession()
+        let amKit = AppleMusicKit(session: mockSession, dispatchQueue: .main)
+        
+        
+        mockSession.response = HTTPURLResponse(url: URL(string: "www.apple.com")!, statusCode: 400, httpVersion: nil, headerFields: nil)
+        let mockRequest: URLRequest = URLRequest(url: URL(string: "www.apple.com")!)
+        
+        let fileUrlString: String? = Bundle.module.path(forResource: "Test", ofType: "json")
+        let fileUrl: URL = URL(fileURLWithPath: fileUrlString!)
+        let data: Data = try! Data(contentsOf: fileUrl)
+        mockSession.data = data
+        
+        var amApiError: AppleMusicError! = nil
+        amKit.performRequestforVoidResponse(request: mockRequest) { (result: Result<Void, AppleMusicKitError>) in
+            switch result {
+            case .success: break
+            case .failure(let error):
+                switch error {
+                case .api(let apiError): amApiError = apiError
+                default: break
+                }
+            }
+        }
+        
+        XCTAssertTrue(amApiError?.status == "404")
+        XCTAssertTrue(amApiError?.title == "Invalid resource")
+    }
 }
+
+private let errorJSON = """
+{
+    "id": "1".
+    "code": "002",
+    "status": 404,
+    "title": "Invalid resource",
+    "detail": "Resource not found"
+}
+"""
